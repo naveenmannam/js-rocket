bde1fe831ceba668946e11607ad8c2d3a44e9f73
Naveen Mannam <naveen.mananm@hotmail.com>
2017-05-31 18:03:17 -0400

    Added abort launch function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 // Get the variable names
 
 var launch = document.getElementById('launchRocket');
+var abort = document.getElementById('abortRocket');
 var rock = document.getElementById('rock');
 
 
 
 // Add the event listener functions
-function getLaunch(){
+function startLaunch(){
     rock.className = 'rocket flying';
+}
+
+function startLaunch(){
+    rock.className = 'rocket';
 }
 
 
 
 // Add the event listeners
 
-launch.addEventListener('click',getLaunch);
+launch.addEventListener('click',startLaunch);
+abort.addEventListener('click',stopLaunch);
