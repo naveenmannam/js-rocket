d166f294646eb894e8cb2f4e538afc2ba67acfe3
Naveen Mannam <naveen.mananm@hotmail.com>
2017-05-31 18:36:44 -0400

    set the for loop

--- js/main.js
+++ js/main.js
@@ -2,16 +2,22 @@
 
 var launch = document.getElementById('launchRocket');
 var abort = document.getElementById('abortRocket');
+var countdown = document.getElementById('countdown');
 var rock = document.getElementById('rock');
-
+var count = countdown.innerText;
 
 
 // Add the event listener functions
-function startLaunch(){
-    rock.className = 'rocket flying';
+function startLaunch() {
+    for (var i = count; i >= 0; i--) {
+        countdown.innerText = i;
+        if (countdown.innerText == 0) {
+            rock.className = 'rocket flying';
+        }
+    }
 }
 
-function stopLaunch(){
+function stopLaunch() {
     rock.className = 'rocket';
 }
 
@@ -19,5 +25,5 @@ function stopLaunch(){
 
 // Add the event listeners
 
-launch.addEventListener('click',startLaunch);
-abort.addEventListener('click',stopLaunch);
+launch.addEventListener('click', startLaunch);
+abort.addEventListener('click', stopLaunch);
